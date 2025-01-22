c87e712e5e46ae805f3cb36174456aa26e7e2835
admhome <admhome@ya.ru>
2025-01-22 17:58:32 +0300

    sql for news table create

---
+++ sql/db.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `news` (
+    `id` int(11) NOT NULL,
+    `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+    `short_text` text CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+    `full_text` text CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+    `is_deleted` int(11) NOT NULL DEFAULT 0
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+ALTER TABLE `news`
+    ADD PRIMARY KEY (`id`);
